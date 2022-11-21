45dd0d8374a2860d8ccd5af4eacc72e4487f62b8
CHARLESojini <ojinichima@gmail.com>
2022-11-21 12:47:34 +0100

    spc.sh

--- userMGT5.sh
+++ userMGT5.sh
@@ -1,4 +1,5 @@
 # This script will be used to adduseser to the sever
-# This should onlyh be used on linux sever
+# This should only be used on linux sever
 # Please use best practies
 # AUTHOUR OJINI
+# Thanks for joining us
